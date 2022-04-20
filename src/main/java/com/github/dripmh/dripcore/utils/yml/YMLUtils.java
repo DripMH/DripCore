d85198be42f7e1ca4da7e6b7adf7811d7dbb1faf
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-04-19 23:51:45 -0500

    Added the data field and the getData(), getDataUtils() methods

--- src/main/java/com/github/dripmh/dripcore/utils/yml/YMLUtils.java
+++ src/main/java/com/github/dripmh/dripcore/utils/yml/YMLUtils.java
@@ -3,7 +3,11 @@ package com.github.dripmh.dripcore.utils.yml;
 public class YMLUtils {
 
     private static ConfigUtils config = new ConfigUtils();
+    private static DataUtils data = new DataUtils();
 
     public static ConfigUtils getConfigUtils() { return config; }
     public static ConfigUtils getConfig() { return config; }
+
+    public static DataUtils getDataUtils() { return data; }
+    public static DataUtils getData() { return data; }
 }
