f9eeef9725423e4301d06e8bcfb01443a297cf64
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-04-19 22:50:01 -0500

    Created Messages class

---
+++ src/main/java/com/github/dripmh/dripcore/helpers/Messages.java
@@ -0,0 +1,11 @@
+package com.github.dripmh.dripcore.helpers;
+
+import com.github.cyberryan1.cybercore.utils.CoreUtils;
+
+public class Messages {
+
+    public static String PLAYER_REQUIRED = CoreUtils.getColored( "&7This command can only be executed by a player!" );
+    public static String CONSOLE_REQUIRED = CoreUtils.getColored( "&7This command can only be executed by console!" );
+
+    public static String PERMS_DENIED = CoreUtils.getColored( "&cInsufficient permissions!" );
+}
