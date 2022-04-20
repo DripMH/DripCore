b69d6d4b9f567fdc9158937f3214bfa2a116d270
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-04-19 22:50:12 -0500

    Setup CyberCore and reload the config file

--- src/main/java/com/github/dripmh/dripcore/DripCore.java
+++ src/main/java/com/github/dripmh/dripcore/DripCore.java
@@ -1,13 +1,19 @@
 package com.github.dripmh.dripcore;
 
-import org.bukkit.Bukkit;
+import com.github.cyberryan1.cybercore.CyberCore;
+import com.github.cyberryan1.cybercore.utils.VaultUtils;
+import com.github.dripmh.dripcore.utils.yml.YMLUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DripCore extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        Bukkit.broadcastMessage( "hi" );
-        Bukkit.broadcastMessage( "server is online" );
+        // Setup things related to CyberCore
+        CyberCore.setPlugin( this );
+        new VaultUtils();
+
+        // Update and reload config/data files
+        YMLUtils.getConfig().getYMLManager().reloadConfig();
     }
 }
