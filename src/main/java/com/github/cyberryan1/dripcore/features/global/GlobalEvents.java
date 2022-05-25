ec634df6eb684a946477991fc2e2981f80fb2874
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-05-24 19:22:20 -0500

    Give players night vision on join, if enabled in config

--- src/main/java/com/github/cyberryan1/dripcore/features/global/GlobalEvents.java
+++ src/main/java/com/github/cyberryan1/dripcore/features/global/GlobalEvents.java
@@ -5,15 +5,25 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class GlobalEvents implements Listener {
 
     private static boolean HUNGER_ENABLED = YMLUtils.getConfig().getBool( "features.no-hunger" );
+    private static boolean NIGHTVISION_ENABLED = YMLUtils.getConfig().getBool( "features.night-vision" );
 
     @EventHandler
     public void onHungerChange( FoodLevelChangeEvent event ) {
         if ( HUNGER_ENABLED ) {
             event.setFoodLevel( 20 );
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin( PlayerJoinEvent event ) {
+        if ( NIGHTVISION_ENABLED ) {
+            event.getPlayer().addPotionEffect( new PotionEffect( PotionEffectType.NIGHT_VISION, 100000, 10, false, false ) );
         }
     }
 }
