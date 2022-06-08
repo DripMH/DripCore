920107c4dabb190d6474343471f6a68c9eec4ca9
fearinq <fearpayments@gmail.com>
2022-06-08 23:16:15 +0700

    added getCurrentTimestamp() method

--- src/main/java/com/github/cyberryan1/dripcore/utils/Utils.java
+++ src/main/java/com/github/cyberryan1/dripcore/utils/Utils.java
@@ -1,4 +1,8 @@
 package com.github.cyberryan1.dripcore.utils;
 
 public class Utils {
+    // returns the current timestamp, in seconds
+    public static long getCurrentTimestamp() {
+        return System.currentTimeMillis() / 1000;
+    }
 }
