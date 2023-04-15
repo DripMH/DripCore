14fa56f8aa0a972b0b089703209386c50f47baf6
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2023-04-15 14:18:07 -0500

    Prevented /teammsg and /tell

--- src/main/java/com/github/cyberryan1/dripcore/features/anticommand/AntiCommandEvents.java
+++ src/main/java/com/github/cyberryan1/dripcore/features/anticommand/AntiCommandEvents.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class AntiCommandEvents implements Listener {
 
     private static List<String> preventedCommands = new ArrayList<>( List.of(
-            "/pl", "/plugin", "/plugins", "/say", "/me", "/?", "/w", "/whisper", "/help", "/ver", "/?", "/version"
+            "/pl", "/plugin", "/plugins", "/say", "/me", "/?", "/w", "/whisper", "/help", "/ver", "/?", "/version", "/teammsg", "/tell"
     ) );
 
     private static String JUNIOR_MOD_PERM = "";
